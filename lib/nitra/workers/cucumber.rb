87fab653d1d6b54bf17d9ab7ed17052898db6333
SpencerMalone <malone.spencer@gmail.com>
2015-09-24 15:32:00 -0400

    Update cucumber.rb

--- lib/nitra/workers/cucumber.rb
+++ lib/nitra/workers/cucumber.rb
@@ -45,7 +45,7 @@ module Nitra::Workers
         puts @configuration.exceptions_to_retry;
         puts @attempt
         puts @configuration.max_attempts
-        puts cuke_runtime.results.scenarios(:failed)
+        puts cuke_runtime.results.inspect
         if cuke_runtime.failure? && @configuration.exceptions_to_retry && @attempt && @attempt < @configuration.max_attempts &&
            cuke_runtime.results.scenarios(:failed).any? {|scenario| puts "Failure!"; puts scenario.exception.to_s; puts scenario.exception.to_s =~ @configuration.exceptions_to_retry; scenario.exception.to_s =~ @configuration.exceptions_to_retry}
           raise RetryException
