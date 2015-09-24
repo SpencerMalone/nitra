d8928916fb6f8074f53c3b68eb33ebd399e5ee19
SpencerMalone <malone.spencer@gmail.com>
2015-09-24 16:28:54 -0400

    Update master.rb

--- lib/nitra/master.rb
+++ lib/nitra/master.rb
@@ -42,7 +42,8 @@ class Nitra::Master
 
     formatter.finish
     burndown.finish configuration.burndown_report if configuration.burndown_report
-
+    require('pry')
+    binding.pry
     !$aborted && progress.files_completed == progress.file_count && progress.failure_count.zero? && !progress.failure
   end
 
